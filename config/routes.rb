ecc658f5dc3a38a3f7d613bf88b71865062330d9
williamhsu17 <williamhsu17@gmail.com>
2017-01-12 02:06:41 +0800

    add route for todos

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
     root to: "todos#index"
   end
 
+  resources :todos
+
+  root to: "todos#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
